fe87cbaa714e19be9f8e0f43d8ebb59d9458363b
Chris Freeman <cwfreeman@gmail.com>
2011-08-23 22:39:59 -0500

    Added button clicks.

--- game-of-life.clj
+++ game-of-life.clj
@@ -1,32 +1,45 @@
 (import '(javax.swing JFrame))
 (import '(java.awt Canvas))
+(import '(javax.swing.event MouseInputAdapter))
 
-(defn canvas [xs]
+(defn get-point [e]
+    [(quot (.getX e) 10) (quot (.getY e) 10)])
+
+(defn canvas [xs max-h max-w]
 	(let [f (JFrame.)
-				c (proxy [Canvas] []
+                c (proxy [Canvas] []
 			(update [g]
-				(doto g (.clearRect 0 0 300 300))
+				(doto g (.clearRect 0 0 max-h max-w))
 				(doseq [x @xs] (. g fillRect (* 10 (first x)) (* 10 (last x)) 10 10))))]
-        (. f setSize 300 300)
+        (. f setSize max-h max-w)
 	(. f add c)
 	(. f setVisible true)
+        (. c addMouseListener (proxy [MouseInputAdapter] []
+                                  (mouseClicked [e]
+                                                (reset! xs (conj @xs (get-point e))))))
+                                  
 	c))
+
+(defn same-set [xs ys]
+    (and
+          (= 0 (count (clojure.set/difference xs ys)))
+          (= 0 (count (clojure.set/difference ys xs)))))
 
 
 ;; Looping 'n' times with state:
-(defn game-loop [xs f n canvas]
-	(.repaint canvas)
-	(reset! xs (f @xs))
-	(Thread/sleep 1000)
-	(when (> n 0)
-		(recur xs f (- n 1) canvas)))
-
+(defn game-loop [sleep-time]
+    (fn [xs f n canvas]
+        (let [old-xs @xs]
+          (.repaint canvas)
+          (reset! xs (f @xs))
+          (Thread/sleep sleep-time)
+          (when (not (same-set @xs old-xs))
+                  (recur xs f (- n 1) canvas)))))
 
 ;; Getting into Rules
-
 (defn in-range [p]
 	(let [x (first p) y (last p)]
-		(and (>= x 0) (>= y 0) (< x 301) (< x 301))))
+		(and (>= x 0) (>= y 0) (< x 1000) (< x 1000))))
 
  (defn neighbors [p]
 	(let [x (first p) x_1 (- x 1) x1 (+ x 1)
@@ -59,5 +72,5 @@
 		(set (regens world))))
 
 (let [xs (atom #{[0 2] [1 3] [2 3] [2 2] [2 1]})
-		c (canvas xs)]
-	(game-loop xs next-gen 20 c))
+		c (canvas xs 1000 1000)]
+	((game-loop 150) xs next-gen 20 c))
